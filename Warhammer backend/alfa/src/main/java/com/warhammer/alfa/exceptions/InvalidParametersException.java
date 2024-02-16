0bc716683293cf35b60cedc9b818423d908de978
Wierzkac <wierzkac@gmail.com>
2024-02-16 16:46:23 +0100

    Added an Exception for attempting to create an object with invalid parameters

---
+++ Warhammer backend/alfa/src/main/java/com/warhammer/alfa/exceptions/InvalidParametersException.java
@@ -0,0 +1,7 @@
+package com.warhammer.alfa.exceptions;
+
+public class InvalidParametersException extends Exception {
+    public InvalidParametersException(String errorMessage) {
+        super(errorMessage);
+    }
+}
