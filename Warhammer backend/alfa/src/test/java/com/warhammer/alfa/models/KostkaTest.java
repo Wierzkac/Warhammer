94f190cd1a71a3f3aca18674173a0947ed0224ce
Wierzkac <wierzkac@gmail.com>
2024-03-01 23:07:55 +0100

    Fix to the KostkaTest for app to be properly build

--- Warhammer backend/alfa/src/test/java/com/warhammer/alfa/models/KostkaTest.java
+++ Warhammer backend/alfa/src/test/java/com/warhammer/alfa/models/KostkaTest.java
@@ -1,6 +1,7 @@
 package com.warhammer.alfa.models;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class KostkaTest {
@@ -8,10 +9,16 @@ public class KostkaTest {
     @Test
     void testValidInput() {
         Kostka dice = new Kostka();
-        assertEquals(9, dice.roll("1k10"), "Result should be between 1 and 10");
-        assertEquals(5, dice.roll("1k6"), "Result should be between 1 and 6");
-        assertEquals(7, dice.roll("2k4"), "Result should be between 2 and 8");
-        assertEquals(15, dice.roll("3k5"), "Result should be between 3 and 15");
+
+        int result;
+        result = dice.roll("1k10");
+        assertTrue(1 <= result && result <= 10, "Result should be between 1 and 10");
+        result = dice.roll("1k6");
+        assertTrue(1 <= result && result <= 6, "Result should be between 1 and 6");
+        result = dice.roll("2k4");
+        assertTrue(2 <= result && result <= 8, "Result should be between 2 and 8");
+        result = dice.roll("3k5");
+        assertTrue(3 <= result && result <= 15, "Result should be between 3 and 15");
     }
 
     @Test
