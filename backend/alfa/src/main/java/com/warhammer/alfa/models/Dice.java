15ca6b00cdf484e04dab1d1d3217173f353966bb
kwierzbicki <kwierzbicki@sii.pl>
2025-05-26 01:09:10 +0200

    Adding another form of dice rolls

--- backend/alfa/src/main/java/com/warhammer/alfa/models/Dice.java
+++ backend/alfa/src/main/java/com/warhammer/alfa/models/Dice.java
@@ -3,29 +3,33 @@ package com.warhammer.alfa.models;
 import java.util.Random;
 
 public class Dice {
-    private Random random;
+    private static final Random random= new Random();
+    private static final String invalidDiceFormatErrorMessage = "Invalid dice format. Use {numberOfRolls}k{valueOnTheDice}";
+    private static final String onlyPositiveErrorMessage = "Number of rolls and value on the dice must be positive integers.";
 
-    public Dice() {
-        random = new Random();
-    }
-
-    public int roll(String version) {
-        String[] parts = version.split("k");
+    public static int roll(String version) {
+        String[] parts;
+        if (version.contains("k"))
+            parts = version.split("k");
+        else if (version.contains("d"))
+            parts = version.split("d");
+        else
+            throw new IllegalArgumentException(invalidDiceFormatErrorMessage);
         if (parts.length != 2) {
             // Handling wrong format
-            throw new IllegalArgumentException("Invalid dice format. Use {numberOfRolls}k{valueOnTheDice}");
+            throw new IllegalArgumentException(invalidDiceFormatErrorMessage);
         }
         int numberOfRolls = Integer.parseInt(parts[0]);
         int valueOnTheDice = Integer.parseInt(parts[1]);
 
         if (numberOfRolls <= 0 || valueOnTheDice <= 0) {
             // Handling numbers below 0
-            throw new IllegalArgumentException("Number of rolls and value on the dice must be positive integers.");
+            throw new IllegalArgumentException(onlyPositiveErrorMessage);
         }
 
         int result = 0;
         for (int i = 0; i < numberOfRolls; i++) {
-            result += random.nextInt(valueOnTheDice) + 1; // Adding 1 because Random.nextInt returns [0, value)
+            result += random.nextInt(valueOnTheDice) + 1;
         }
         return result;
     }
