9376ee84c7551f7ba8cce98b46d9e1e60e994b8d
kwierzbicki <kwierzbicki@sii.pl>
2025-05-29 15:58:49 +0200

    Dice string formatting fix

--- backend/alfa/src/main/java/com/warhammer/alfa/models/Dice.java
+++ backend/alfa/src/main/java/com/warhammer/alfa/models/Dice.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Dice {
     private static final Random random= new Random();
-    private static final String invalidDiceFormatErrorMessage = "Invalid dice format. Use {numberOfRolls}k{valueOnTheDice}";
-    private static final String onlyPositiveErrorMessage = "Number of rolls and value on the dice must be positive integers.";
+    private static final String INVALID_DICE_FORMAT_ERROR_MESSAGE = "Invalid dice format. Use {numberOfRolls}k{valueOnTheDice}";
+    private static final String ONLY_POSITIVE_ERROR_MESSAGE = "Number of rolls and value on the dice must be positive integers.";
 
     public static int roll(String version) {
         String[] parts;
@@ -14,17 +14,17 @@ public class Dice {
         else if (version.contains("d"))
             parts = version.split("d");
         else
-            throw new IllegalArgumentException(invalidDiceFormatErrorMessage);
+            throw new IllegalArgumentException(INVALID_DICE_FORMAT_ERROR_MESSAGE);
         if (parts.length != 2) {
             // Handling wrong format
-            throw new IllegalArgumentException(invalidDiceFormatErrorMessage);
+            throw new IllegalArgumentException(INVALID_DICE_FORMAT_ERROR_MESSAGE);
         }
         int numberOfRolls = Integer.parseInt(parts[0]);
         int valueOnTheDice = Integer.parseInt(parts[1]);
 
         if (numberOfRolls <= 0 || valueOnTheDice <= 0) {
             // Handling numbers below 0
-            throw new IllegalArgumentException(onlyPositiveErrorMessage);
+            throw new IllegalArgumentException(ONLY_POSITIVE_ERROR_MESSAGE);
         }
 
         int result = 0;
